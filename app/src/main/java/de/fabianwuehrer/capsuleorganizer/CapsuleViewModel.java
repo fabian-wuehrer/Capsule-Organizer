df982020b88286f6a3573d6801e2f1c14b273192
Fabian Wührer <fabian.wuehrer@tum.de>
2018-12-21 08:31:49 +0100

    created CapsuleViewModel

---
+++ app/src/main/java/de/fabianwuehrer/capsuleorganizer/CapsuleViewModel.java
@@ -0,0 +1,37 @@
+package de.fabianwuehrer.capsuleorganizer;
+
+import android.app.Application;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+public class CapsuleViewModel extends AndroidViewModel {
+
+    private CapsuleRepository repository;
+    private LiveData<List<Capsule>> allCapsules;
+
+    public CapsuleViewModel(@NonNull Application application) {
+        super(application);
+        repository = new CapsuleRepository(application);
+        allCapsules = repository.getAllCapsules();
+    }
+
+    public void insert(Capsule capsule){
+        repository.insert(capsule);
+    }
+
+    public void update(Capsule capsule){
+        repository.update(capsule);
+    }
+
+    public void delete(Capsule capsule){
+        repository.delete(capsule);
+    }
+
+    public LiveData<List<Capsule>> getAllCapsules() {
+        return allCapsules;
+    }
+}
