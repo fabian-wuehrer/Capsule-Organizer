12b6c05a6a838f5bfe01933f3401e15e41a3827c
Fabian Wührer <fabian.wuehrer@tum.de>
2018-12-21 09:39:18 +0100

    updated MainActivity (added CapsuleAdapter and more)

--- app/src/main/java/de/fabianwuehrer/capsuleorganizer/MainActivity.java
+++ app/src/main/java/de/fabianwuehrer/capsuleorganizer/MainActivity.java
@@ -1,12 +1,16 @@
 package de.fabianwuehrer.capsuleorganizer;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,11 +21,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        final CapsuleAdapter adapter = new CapsuleAdapter();
+        recyclerView.setAdapter(adapter);
+
         capsuleViewModel = ViewModelProviders.of(this).get(CapsuleViewModel.class);
         capsuleViewModel.getAllCapsules().observe(this, new Observer<List<Capsule>>() {
             @Override
             public void onChanged(List<Capsule> capsules) {
-
+                adapter.submitList(capsules);
             }
         });
     }
