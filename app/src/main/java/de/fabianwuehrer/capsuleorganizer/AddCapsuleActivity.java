19bb8540d51fc88db15926ceb4cd82bbaa198e21
Fabian Wührer <fabian.wuehrer@tum.de>
2018-12-21 10:54:12 +0100

    added AddCapsuleActivity and activity_add_capsule

---
+++ app/src/main/java/de/fabianwuehrer/capsuleorganizer/AddCapsuleActivity.java
@@ -0,0 +1,14 @@
+package de.fabianwuehrer.capsuleorganizer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddCapsuleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_capsule);
+    }
+}
