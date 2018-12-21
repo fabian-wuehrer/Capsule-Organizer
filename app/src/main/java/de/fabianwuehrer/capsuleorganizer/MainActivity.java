a5d00759eaaaa4ad9d5d75ad9f9c0346f404c91b
Fabian Wührer <fabian.wuehrer@tum.de>
2018-12-21 08:48:18 +0100

    created capsule_item View

--- app/src/main/java/de/fabianwuehrer/capsuleorganizer/MainActivity.java
+++ app/src/main/java/de/fabianwuehrer/capsuleorganizer/MainActivity.java
@@ -2,13 +2,27 @@ package de.fabianwuehrer.capsuleorganizer;
 
 import android.os.Bundle;
 
+import java.util.List;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CapsuleViewModel capsuleViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        capsuleViewModel = ViewModelProviders.of(this).get(CapsuleViewModel.class);
+        capsuleViewModel.getAllCapsules().observe(this, new Observer<List<Capsule>>() {
+            @Override
+            public void onChanged(List<Capsule> capsules) {
+
+            }
+        });
     }
 }
