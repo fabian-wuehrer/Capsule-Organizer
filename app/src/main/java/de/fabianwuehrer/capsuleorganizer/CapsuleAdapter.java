51c2a553ac2c1cb59fa6d11ecad4bd97df759cc0
Fabian Wührer <fabian.wuehrer@tum.de>
2018-12-22 22:29:16 +0100

    code cleanup

--- app/src/main/java/de/fabianwuehrer/capsuleorganizer/CapsuleAdapter.java
+++ app/src/main/java/de/fabianwuehrer/capsuleorganizer/CapsuleAdapter.java
@@ -3,7 +3,6 @@ package de.fabianwuehrer.capsuleorganizer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.DiffUtil;
